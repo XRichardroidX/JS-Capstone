19320db46ae3988964f8089a6e53a8820674d3e1
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-01-03 20:22:41 +0000

    Hook a click event listener up with the comments btn

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ function getData() {
                 <h2 class="watch-name">${index.name}</h2>
                 <h4 class="full-date"><b class="date">Date:</b>${index.airdate}</h4>
                 <p class="full-summary"><b class="summary">Summary:</b>${removeTags(index.summary)}</p>
-                    <button class="btn">Comment</button>
+                    <button class="btn" data-comment-btn>Comment</button>
         `;
         allCards.appendChild(card);
       });
@@ -29,3 +29,14 @@ function getData() {
 }
 
 getData();
+
+window.addEventListener('load', () => {
+  const moviesWrapper = document.querySelector('[data-movies-wrapper]');
+  moviesWrapper.addEventListener('click', (e) => {
+    const isCommentsBtn = e.target.hasAttribute('data-comment-btn');
+
+    if (isCommentsBtn) {
+      console.log('Comments btn clicked');
+    }
+  });
+});
