e09ab2c33e8506fcdcce1a531e8a24f388929d43
XRichardroidX <oguzieibehrichard@gmail.com>
2023-01-03 16:51:48 +0100

    Finish Home page

--- src/index.js
+++ src/index.js
@@ -1,48 +1,33 @@
 import './style.css';
 
 const allCards = document.querySelector('.all-cards');
-
-const show = [
-  {
-    imageURL: 'https://static.tvmaze.com/uploads/images/original_untouched/1/4392.jpg',
-    name: 'Watch Pilot',
-    date: '2013-06-24',
-    summary: "The Chester's Mill residents receive an unexpected visit from their loved ones on the other side. Meanwhile, the community braces for a threat from outside the Dome.",
-  },
-  {
-    imageURL: 'https://static.tvmaze.com/uploads/images/original_untouched/1/4392.jpg',
-    name: 'Watch Pilot',
-    date: '2013-06-24',
-    summary: "The Chester's Mill residents receive an unexpected visit from their loved ones on the other side. Meanwhile, the community braces for a threat from outside the Dome.",
-  },
-  {
-    imageURL: 'https://static.tvmaze.com/uploads/images/original_untouched/1/4392.jpg',
-    name: 'Watch Pilot',
-    date: '2013-06-24',
-    summary: "The Chester's Mill residents receive an unexpected visit from their loved ones on the other side. Meanwhile, the community braces for a threat from outside the Dome.",
-  },
-  {
-    imageURL: 'https://static.tvmaze.com/uploads/images/original_untouched/1/4392.jpg',
-    name: 'Watch Pilot',
-    date: '2013-06-24',
-    summary: "The Chester's Mill residents receive an unexpected visit from their loved ones on the other side. Meanwhile, the community braces for a threat from outside the Dome.",
-  },
-];
+function removeTags(str) {
+  if ((str === null) || (str === '')) return false;
+  str = str.toString();
+  return str.replace(/(<([^>]+)>)/ig, '');
+}
 
 let card;
 
-const display = () => {
-  show.forEach((index) => {
-    card = document.createElement('div');
-    card.className = 'card';
-    card.innerHTML = `
+function getData() {
+  fetch('https://api.tvmaze.com/shows/1/episodes')
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res[1]);
+
+      res.forEach((index) => {
+        card = document.createElement('div');
+        card.className = 'card';
+        card.innerHTML = `
+                <img src=${index.image.original} alt="">
                 <h2 class="watch-name">${index.name}</h2>
-                <h4 class="full-date"><b class="date">Date:</b>${index.date}</h4>
-                <p class="full-summary"><b class="summary">Summary:</b>${index.summary}</p>
+                <h4 class="full-date"><b class="date">Date:</b>${index.airdate}</h4>
+                <p class="full-summary"><b class="summary">Summary:</b>${removeTags(index.summary)}</p>
                     <button class="btn">Comment</button>
         `;
-    allCards.appendChild(card);
-  });
-};
+        allCards.appendChild(card);
+      });
+    });
+}
 
-display();
+getData();
