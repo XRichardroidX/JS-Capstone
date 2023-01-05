a35cc56ec2d7c13b79721248dc2a70ace8d74a06
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-01-05 16:13:48 +0000

    Modify look and feel of UI

--- src/module/displayDataInModal.js
+++ src/module/displayDataInModal.js
@@ -1,7 +1,11 @@
 const imgSrc = document.querySelector('[data-modal-image]');
 const title = document.querySelector('[data-show-title]');
-const premierDate = document.querySelector('[data-show-date]');
+const premierDate = document.querySelector('[data-premiered]');
 const modalContent = document.querySelector('[data-modal-content]');
+const showLanguage = document.querySelector('[data-language]');
+const showStatus = document.querySelector('[data-status]');
+const showGenres = document.querySelector('[data-genres]');
+const showSummary = document.querySelector('[data-show-summary]');
 
 const displayDataInModal = (shows, index) => {
   const show = shows[index - 1];
@@ -10,6 +14,10 @@ const displayDataInModal = (shows, index) => {
   imgSrc.setAttribute('src', `${show.image.original}`);
   title.innerText = show.name;
   premierDate.innerText = show.premiered;
+  showLanguage.innerText = show.language;
+  showStatus.innerText = show.status;
+  showGenres.innerText = show.genres;
+  showSummary.innerHTML = show.summary;
 };
 
 export default displayDataInModal;
