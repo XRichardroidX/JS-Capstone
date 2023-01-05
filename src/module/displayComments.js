5fcdf60f09ccc239ca45beba1e5fca6fa9b83e54
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-01-05 19:47:51 +0000

    Fix comments count in popup modal

--- src/module/displayComments.js
+++ src/module/displayComments.js
@@ -2,10 +2,12 @@ import commentsBuilder from './commentsBuilder.js';
 import getCommentsDataAsync from './getCommentsDataAsync.js';
 
 const commentsWrapper = document.querySelector('[data-comments-list]');
+const commentsCountWrapper = document.querySelector('[data-tv-show-comments]');
 
 const displayComments = async (index) => {
   commentsWrapper.innerHTML = '';
   const comments = await getCommentsDataAsync(index);
+  commentsCountWrapper.innerText = comments.length;
 
   comments.forEach((comment) => {
     commentsWrapper.insertAdjacentHTML('beforeend', commentsBuilder(comment));
